f51413e072d49a7e30d7ada85de80cc7ac2d60a0
Aime Serge <aimeserge51260@gmail.com>
2026-04-24 10:53:58 +0200

    feat: add implementation for student profile program

---
+++ student_profile.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+// This program displays the profile of a student in a formatted display 
+// This program is written in C language 
+//This program use only function "Main" to display my student profile information on the screen
+//This program use formatting structure "\n" to display the information in a formatted way on the screen
+int main() {
+    //Openning the main function of the program
+
+    // displaying my last name without new line
+    printf("Full Name       : Ukobizaba ");
+
+    // attaching and displaying my first name to the last name 
+    printf("Aime Serge\n");
+
+    // Displaying my department and year of study
+    printf("Department : Computer Science_Year One\n");
+
+    // Displaying my school
+    printf("School     : School of ICT\n");
+
+    // Displaying my college
+    printf("College    : UR_CST\n");
+
+    // printing my registration number
+    printf("Reg No     : 225039469\n");
+    // Terminating the program
+    return 0;
+}// End of the main function
