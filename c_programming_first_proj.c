df2ec850a78882f9e35a36a67abebad3c2ed111e
Aime Serge <aimeserge51260@gmail.com>
2026-04-24 10:52:11 +0200

    feat: add source code for first C project

---
+++ c_programming_first_proj.c
@@ -0,0 +1,20 @@
+//This my first c programming project, I am working the displaying message project.
+#include <stdio.h>
+int main() {
+    printf("Hey, C Engineers!\n");
+    printf("\n");
+    printf("It's Aime Serge, from Rwanda. I'm an ALX Ventures Rwanda Ambassador!\n");
+    printf("\n");
+    printf("I am excited to learn C programming!\n");
+    printf("I am looking forward to building amazing projects with C language.\n");
+    printf("I can't stop! I will keep learning, building and engaging in C community programs");
+    printf("\n");
+    printf("Thank you for your support and encouragement!\n");
+    printf("Let's code together!\n");
+    printf("\n");
+    printf("Best regards,\n");
+    printf("Aime Serge\n");
+    printf("\n");
+    printf("Just thumbs up! And say, \"You're welcome to learn C programming!\"\n");
+    return 0;
+}
