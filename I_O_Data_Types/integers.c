fb5d5557d5693cce7720bd4ec5abe40e836a0d0b
Aime Serge <aimeserge51260@gmail.com>
2026-04-24 10:57:52 +0200

    feat: add integer data types and I/O handling implementation

---
+++ I_O_Data_Types/integers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter an integer number\n");
+    printf("\n");
+    scanf("%d", &a);
+    printf("The number you entered is %d", a);
+    printf("\n");
+    printf("Number=%d", a);
+        return 0;
+}
